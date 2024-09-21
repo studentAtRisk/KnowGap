6054e73cdd1e840ac3fbcd94cda2d3b23fe7254b
ramird21 <ramirdalencour@gmail.com>
2024-09-21 15:17:07 -0400

    cleaned up backend code

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-# Come back and import the stuff here
 from youtubesearchpython import VideosSearch
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
 import json
 
+# Handling Environment Variables
 load_dotenv()
 API_KEY = os.getenv('OPENAI_KEY')
+
 client = OpenAI(api_key=API_KEY)
 
 from flask import Flask, request, jsonify
@@ -14,26 +15,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Jason!'
+    return jsonify('Hello Jason!')
 
-@app.route('/getvideorec')
-def get_video_recommendation(): # ==== Add in async
-    student_data = request.get_json() # Get Quiz info
-    
-    topics_final =  process_student(student_data) # Put in await
-    
-    query =  generate_core_topic(topics_final) # Put in await
-    
-    print(f"Query is: {query}")
+@app.route('/getvideorec', methods=['GET'])
+def get_video_recommendation():
+    student_data = request.get_json()
+    topics_final =  process_student(student_data)
+    query =  generate_core_topic(topics_final)
     
     # Check if query is a string or list and process accordingly
     if isinstance(query, str):
         query = query.splitlines()
-        
-    print(f"Query after conversion is: {query}")
-        
-    
-    print(f"Second Debugging print: {type(query)}")
         
     # Now clean up each topic by removing index numbers and unnecessary whitespace
     topic_list = [item.split(". ", 1)[1] if ". " in item else item for item in query]
@@ -55,7 +47,7 @@ def get_video_recommendation(): # ==== Add in async
 # ================================================================================================================================================
 # Beyond this point, those functions are helper function
 # Function to generate a core topic using OpenAI
-def generate_core_topic(question_data): # ==== Add in async
+def generate_core_topic(question_data):
     prompt = f"From the following question and answer data, extract a unique core topic from each question to study: {question_data}"
     response = client.completions.create(
         prompt=prompt,
@@ -71,7 +63,7 @@ def generate_core_topic(question_data): # ==== Add in async
     return core_topic
 
 # Function to generate a YouTube query using OpenAI
-def generate_youtube_query(topic): # ==== Add in async
+def generate_youtube_query(topic):
     prompt = f"Generate a broad YouTube search query for the topic: {topic}."
     
     # Generating completion using OpenAI client
@@ -88,7 +80,7 @@ def generate_youtube_query(topic): # ==== Add in async
     return youtube_query
 
 # Function to process each student's data
-def process_student(student_data): # ==== Add in async
+def process_student(student_data):
     topics = [question['question'] for question in student_data['questions']]
     return topics
 
