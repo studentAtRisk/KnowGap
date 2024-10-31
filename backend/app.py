f78344c84d2310c9ac6ec6974db63232e91dae11
Jason Saini <jasonsaini.exe@gmail.com>
2024-10-31 16:35:31 -0400

    Fixed cors typo

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 from quart import Quart
-from quart_cors import CORS
+from quart_cors import cors
 from routes.base_routes import init_base_routes
 from routes.video_routes import init_video_routes
 from routes.support_routes import init_support_routes
 from routes.course_routes import init_course_routes
 
 app = Quart(__name__)
-CORS(app, resources={
+cors(app, resources={
     r"/*": {
         "origins": ["https://canvas.instructure.com/"],
         "methods": ["GET", "POST", "OPTIONS"],
