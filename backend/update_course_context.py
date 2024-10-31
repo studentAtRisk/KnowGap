2328c75bcdaea3a6334e1e9a7cb6a2132af3e8fe
Jason Saini <jasonsaini.exe@gmail.com>
2024-10-30 22:04:46 -0400

    Update course context async'd

--- backend/update_course_context.py
+++ backend/update_course_context.py
@@ -12,13 +12,13 @@ DB_CONNECTION_STRING = os.getenv('DB_CONNECTION_STRING')
 mdb_client = MongoClient(DB_CONNECTION_STRING)
 db = mdb_client['KnowGap']
 
-def update_context(course_id, course_context):
+async def update_context(course_id, course_context):
     try:
         # Connect to MongoDB
         course_context_collection = db['Course Contexts']
 
         # Update or insert the course context
-        course_context_collection.update_one(
+        await course_context_collection.update_one(
             {'courseid': course_id},
             {
                 '$set': {
