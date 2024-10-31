1ea4c19875aba3beea7bd47afafb23838fd9679f
Jason Saini <jasonsaini.exe@gmail.com>
2024-10-31 16:30:26 -0400

    Added resources to quart_cors

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,18 @@
 from quart import Quart
-from quart_cors import cors
+from quart_cors import CORS
 from routes.base_routes import init_base_routes
 from routes.video_routes import init_video_routes
 from routes.support_routes import init_support_routes
 from routes.course_routes import init_course_routes
 
 app = Quart(__name__)
-cors(app)
-
+CORS(app, resources={
+    r"/*": {
+        "origins": ["https://canvas.instructure.com/"],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 init_base_routes(app)
 init_course_routes(app)
 init_video_routes(app)
