4912b72160843eec5eff6da7c501c059c2ee6556
Jason Saini <jasonsaini.exe@gmail.com>
2024-10-31 17:57:03 -0400

    Updated cors args for FrOnTeNd

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,11 @@ from routes.support_routes import init_support_routes
 from routes.course_routes import init_course_routes
 
 app = Quart(__name__)
-cors(app, allow_origin="*", allow_headers = ["content-type", "Authorization"], allow_methods=["GET", "POST", "OPTIONS"] )
+cors(app, 
+     allow_origin="*",
+     allow_headers=["Content-Type", "Authorization", "X-Requested-With"],
+     allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+     allow_credentials=True)
 
 init_base_routes(app)
 init_course_routes(app)
