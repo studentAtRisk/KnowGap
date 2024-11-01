57d2940d8fbf86b81ca5c846ccfd718eab2129e1
Jordan Morillo <82860617+Jordan917222@users.noreply.github.com>
2024-11-01 17:07:17 -0400

    Update course_utils.py, include get quizzes function both update files use.

--- backend/utils/course_utils.py
+++ backend/utils/course_utils.py
@@ -16,3 +16,52 @@ def parse_date(date_str):
 def clean_text(text):
     # Normalize and filter to keep only ASCII characters
     return ''.join(char for char in text if ord(char) < 128)
+
+async def get_quizzes(courseid, access_token, link):
+
+
+    api_url = f'https://{link}/api/v1/courses/{courseid}/quizzes?per_page=100'
+    headers ={
+        'Authorization': f'Bearer {access_token}'
+    }
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url, headers=headers) as response:
+                if response.status == 200:
+                    unfiltered_data = await response.json()
+                    max_date = datetime.now(timezone.utc)
+                    # Sorts data by most recent unlocked quiz that is published to oldest
+
+                    # Filter and sort the data
+                    print(unfiltered_data)
+
+
+                    data = sorted(
+                        [
+                            item for item in unfiltered_data
+                            if item["published"] and item["all_dates"][0]["unlock_at"] and parse_date(item["all_dates"][0]["unlock_at"]) <= max_date
+                        ],
+                        key=lambda x: (
+                            # Sort by unlock date in descending order (later dates first)
+                            parse_date(x["all_dates"][0]["unlock_at"])
+                        ),
+                        reverse=True  # Sorting in descending order to get later dates first
+                    )
+
+                    
+                    
+                    quizlist = []
+                    quizname = []
+                    
+                    for x in range(min(len(data), global_amtofquizzes)):
+                        quizlist.append(data[x]["id"])
+                        quizname.append(data[x]["title"])
+
+                    print(quizname)
+                    return quizlist, quizname 
+                else:
+                    print("did we error here?")
+                    return {'error': f'Failed to fetch data from API: {response.text}'}, response.status
+    except Exception as e:
+        print(e)
+        return {'error' : str(e)}, 500
